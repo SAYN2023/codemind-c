a84dc534d6a15177bba9fd482045fca2729c1d06
SAYN2023 <151954905+SAYN2023@users.noreply.github.com>
2023-11-28 00:27:44 +0530

    #include<stdio.h> int main(){     int cp,sp;     scanf("%d %d ",&cp,&sp);     if(cp<sp)     printf("Profit");     else if(cp==sp)     printf("No Profit and No Loss");     else     printf("Loss"); }

---
+++ Profit_or_Loss.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int cp,sp;
+    scanf("%d %d ",&cp,&sp);
+    if(cp<sp)
+    printf("Profit");
+    else if(cp==sp)
+    printf("No Profit and No Loss");
+    else
+    printf("Loss");
+}
